f0cc3361a4c87dea0382e6186a5c3137893f3afb
Hanna Mlenarz <han15644nammlenarz@users.noreply.github.com>
2026-04-21 07:51:08 +0800

    chore: expose Pipeline and ParallelPipeline at top level for convenience

--- src/agentscope/__init__.py
+++ src/agentscope/__init__.py
@@ -8,9 +8,14 @@ from agentscope.agents import AgentBase
 from agentscope.models import ModelWrapperBase
 from agentscope.pipelines import SequentialPipeline
 
+# Personal note: also expose Pipeline and ParallelPipeline since I use them frequently
+from agentscope.pipelines import Pipeline, ParallelPipeline
+
 __all__ = [
     "__version__",
     "AgentBase",
     "ModelWrapperBase",
+    "Pipeline",
+    "ParallelPipeline",
     "SequentialPipeline",
 ]
